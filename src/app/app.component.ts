8833cae9a2930d5b93791178773ac0a4d8ea8f0b
kiran-0407 <kiran.kaur@knoldus.com>
2023-09-14 17:44:16 +0530

    animation example in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('changeDivStyle', [
+      state('start', style({
+        width: '150px',
+        height:'100px',
+        background:'blue',
+        transform:'translateX(0)'
+      })),
+      state('end', style({
+        width: '300px',
+        height:'150px',
+        background:'green',
+        transform:'translateY(50%)'
+      })),
+      transition('start<=>end', animate('1500ms'))
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'animationsInAngular';
+  initialState='start';
+
+  toggleState() {
+    this.initialState = this.initialState === 'start' ? 'end' : 'start';
+  }
 }
